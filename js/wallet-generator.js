a9eeaa815116ac7141a74fe7aaf047f78d9146d7
sanlui <elbuizx@gmail.com>
2025-07-24 21:21:35 +0200

    Update and rename countdown.js to wallet-generator.js

---
+++ js/wallet-generator.js
@@ -0,0 +1,9 @@
+function generateWallet() {
+  const bitcoin = window.bitcoinjs;
+  const keyPair = bitcoin.ECPair.makeRandom();
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+  const privateKeyWIF = keyPair.toWIF();
+
+  document.getElementById("btc-address").innerText = address;
+  document.getElementById("btc-private").innerText = privateKeyWIF;
+}
