25032cb2de7ef6eeb29c838827f533c1cc3ee1e2
sanlui <elbuizx@gmail.com>
2025-07-24 21:36:47 +0200

    Update wallet-generator.js

--- js/wallet-generator.js
+++ js/wallet-generator.js
@@ -1,8 +1,16 @@
+// wallet-generator.js
+
 function generateWallet() {
+  // Usa direttamente l'oggetto bitcoin caricato da CDN
   const keyPair = bitcoin.ECPair.makeRandom();
+
+  // Genera l'indirizzo Bitcoin P2PKH (legacy)
   const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+
+  // Ottieni la chiave privata in formato WIF (Wallet Import Format)
   const privateKeyWIF = keyPair.toWIF();
 
+  // Mostra indirizzo e chiave privata nel DOM
   document.getElementById("btc-address").innerText = address;
   document.getElementById("btc-private").innerText = privateKeyWIF;
 }
