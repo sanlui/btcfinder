0f2e0ed931273c814c9756b21ed70a5404e97fa9
sanlui <elbuizx@gmail.com>
2025-07-24 22:25:48 +0200

    Update wallet-generator.js

--- js/wallet-generator.js
+++ js/wallet-generator.js
@@ -1,11 +1,11 @@
 function generateWallet() {
   try {
-    if (typeof bitcoinjs === "undefined") {
+    if (typeof bitcoin === "undefined") {
       throw new Error("bitcoinjs-lib non caricato correttamente");
     }
 
-    const keyPair = bitcoinjs.ECPair.makeRandom();
-    const { address } = bitcoinjs.payments.p2pkh({ pubkey: keyPair.publicKey });
+    const keyPair = bitcoin.ECPair.makeRandom();
+    const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
     const privateKeyWIF = keyPair.toWIF();
 
     document.getElementById('btc-address').textContent = address;
