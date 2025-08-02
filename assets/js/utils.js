1ecf70ab81f29299d27b98197e57d85485b139d3
sanlui <elbuizx@gmail.com>
2025-08-03 00:10:19 +0200

    g
    g

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,21 +1,5 @@
-export function formatBTC(satoshi) {
-  return (satoshi / 1e8).toFixed(8) + ' BTC';
-}
-
-export function formatNumber(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-export function shortenHash(hash) {
-  return hash.length > 20 ? `${hash.substring(0, 10)}...${hash.substring(hash.length - 10)}` : hash;
-}
-
-export function copyToClipboard(text, elementId = 'copy-feedback') {
-  navigator.clipboard.writeText(text).then(() => {
-    const btn = document.getElementById(elementId);
-    if (btn) {
-      btn.textContent = 'Copied!';
-      setTimeout(() => btn.textContent = 'Copy', 2000);
-    }
-  });
+export async function fetchBTCPrice() {
+  const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+  const data = await res.json();
+  return data.bitcoin.usd;
 }
