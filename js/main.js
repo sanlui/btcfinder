91f78e9771608584a065df14865d67ebcfcdbf59
sanlui <elbuizx@gmail.com>
2025-08-02 22:20:12 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function generateWallet() {
-  // Genera una nuova chiave privata e indirizzo BTC
-  const keyPair = bitcoin.ECPair.makeRandom();
-  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
-  const privateKey = keyPair.toWIF();
-
-  document.getElementById("btc-address").textContent = address;
-  document.getElementById("btc-private").textContent = privateKey;
-}
