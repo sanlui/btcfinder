b1bb60e6797bf4326b95cddc62331dda976b7502
sanlui <elbuizx@gmail.com>
2025-08-03 18:35:55 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,17 +1,30 @@
 export async function searchBlockchain(type, query) {
   const API_BASE = 'https://blockstream.info/api';
-  const endpoints = {
-    tx: `${API_BASE}/tx/${query}`,
-    address: `${API_BASE}/address/${query}`,
-    block: `${API_BASE}/block/${query}`
-  };
-
+  
   try {
-    const response = await fetch(endpoints[type]);
-    if (!response.ok) throw new Error(`API error: ${response.status}`);
+    let url;
+    switch(type) {
+      case 'address':
+        url = `${API_BASE}/address/${query}`;
+        break;
+      case 'tx':
+        url = `${API_BASE}/tx/${query}`;
+        break;
+      case 'block':
+        url = `${API_BASE}/block/${query}`;
+        break;
+      default:
+        throw new Error('Invalid search type');
+    }
+    
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`API error: ${response.status}`);
+    }
+    
     return await response.json();
   } catch (error) {
-    console.error('API error:', error);
+    console.error('API Error:', error);
     throw error;
   }
 }
