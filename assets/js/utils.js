68fbd4dc88fb970a3e58ae748249d38fbe1c938c
sanlui <elbuizx@gmail.com>
2025-08-03 14:59:30 +0200

    Delete assets directory

--- assets/js/utils.js
+++
@@ -1,5 +0,0 @@
-export async function fetchBTCPrice() {
-  const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
-  const data = await res.json();
-  return data.bitcoin.usd;
-}
