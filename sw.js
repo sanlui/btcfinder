947ca0db55bdf005d654ffbe0cb245052eb8202b
sanlui <elbuizx@gmail.com>
2025-08-06 20:54:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'bitcoin-explorer-v1';
+const CACHE_NAME = 'btc-explorer-v2';
+const OFFLINE_URL = '/404.html';
 const ASSETS = [
   '/',
   '/index.html',
   '/css/styles.css',
   '/js/main.js',
-  '/favicon.ico'
+  '/js/worker.js',
+  '/404.html'
 ];
 
 self.addEventListener('install', (event) => {
@@ -15,8 +17,40 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) return response;
+        
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200) {
+              return caches.match(OFFLINE_URL);
+            }
+            
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, responseToCache));
+              
+            return response;
+          })
+          .catch(() => caches.match(OFFLINE_URL))
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
